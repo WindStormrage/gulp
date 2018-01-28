8a27bbe6b686ab2c44ffd7db7be2a34f9099f5df
xhy <695729033@qq.com>
2018-01-28 23:05:42 +0800

    对js的压缩是可以了，也可以监听动态变化了，但是在html里面没有调用到方法，应该是模块的问题

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,24 @@
 var gulp = require('gulp');
 var runSequence = require('run-sequence');
+var browserify = require('browserify');
+var fs = require('fs');
 
 gulp.task('default',() => {
-    runSequence('indexjs', 'watch')
+    runSequence('mainjs', 'watch')
 })
 
 //处理渲染
-gulp.task('indexjs', ()=>{
-    console.log('666')
+gulp.task('mainjs', ()=>{
+    console.log('处理渲染')
+    //assets里面放的是处理前的静态文件
+    //然后通过处理放在main.js里面
+    browserify().add('./assets/js/index.js').bundle().pipe(fs.createWriteStream('./js/main.js'))
 })
 
 //监听变动
 gulp.task('watch', () => {
-    gulp.watch(['./js/*.js'], ()=>{
+    gulp.watch(['./assets/js/*.js'], ()=>{
         //监听到后就重新渲染
-        runSequence('indexjs')
+        runSequence('mainjs')
     })
 })
